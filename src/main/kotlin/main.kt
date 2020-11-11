0b2d00a311bf9bdb98d515c1a937d6544ed24d8c
ubu <ubuphobos@gmail.com>
2020-11-12 01:23:43 +0200

    basic search panel design

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,22 @@
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 fun main() = Window {
-    var text by remember { mutableStateOf("Say hello!") }
+
+    var text by remember { mutableStateOf("Please enter journal name") }
 
     MaterialTheme(
         colors = MaterialTheme.colors.copy(
@@ -18,10 +27,22 @@ fun main() = Window {
         )
     ) {
         Scaffold {
-            Button(onClick = {
-                text = "Hello user"
-            }) {
-                Text(text)
+            TopAppBar(
+                    backgroundColor = Color(49,49,49),
+                    elevation = 3.dp
+            ) {
+                Row {
+                    TextField(
+                            textStyle = TextStyle(
+                                    color = Color.White,
+                                    fontSize = 13.sp
+                            ),
+                            modifier = Modifier.weight(1f),
+                            maxLines = 1,
+                            onValueChange = { text = it },
+                            value = text
+                    )
+                }
             }
         }
     }
