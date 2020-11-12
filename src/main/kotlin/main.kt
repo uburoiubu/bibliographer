bb6607fe0e600cca7af42b739186bba63e321ed8
ubu <ubuphobos@gmail.com>
2020-11-12 19:13:25 +0200

    fixes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,8 @@
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,8 +14,28 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
+import kotlinx.serialization.parse
+
+val client = HttpClient(CIO) {
+    install(JsonFeature) {
+        serializer = KotlinxSerializer()
+    }
+}
 
 fun main() = Window {
+
+    var result by remember { mutableStateOf("") }
 
     var query by remember { mutableStateOf("") }
 
@@ -25,24 +48,47 @@ fun main() = Window {
         )
     ) {
         Scaffold {
-            TopAppBar(
-                    backgroundColor = Color(49,49,49),
-                    elevation = 3.dp
-            ) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Text(text = "Your search query: ", color = Color.White, fontSize = 12.sp)
-                    TextField(
-                            textStyle = TextStyle(
-                                    color = Color.White,
-                                    fontSize = 12.sp
-                            ),
-                            modifier = Modifier.weight(1f),
-                            maxLines = 1,
-                            onValueChange = { query = it },
-                            value = query
-                    )
+            ScrollableColumn {
+                TopAppBar(
+                        backgroundColor = Color(49,49,49),
+                        elevation = 3.dp
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(text = "Your search query: ", color = Color.White, fontSize = 12.sp)
+                        TextField(
+                                textStyle = TextStyle(
+                                        color = Color.White,
+                                        fontSize = 12.sp
+                                ),
+                                modifier = Modifier.weight(1f),
+                                maxLines = 1,
+                                onValueChange = { query = it },
+                                value = query
+                        )
+                    }
                 }
+                Button(
+                        modifier = Modifier.padding(16.dp),
+                        onClick = { GlobalScope.launch { result = search() } },
+                        content = { Text("Search") }
+                )
+                Text(
+                        modifier = Modifier.padding(16.dp),
+                        text = result,
+                        fontSize = 12.sp,
+                        color = Color.LightGray
+                )
             }
         }
     }
+}
+
+suspend fun search() : String {
+    val response = client.get<String>("http://api.crossref.org/journals")
+    val format = Json {
+        prettyPrint = true
+    }
+    val result = format.parseToJsonElement(response)
+
+    return format.encodeToString(result)
 }
