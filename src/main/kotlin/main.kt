7edccc71f474f88e2727013d998093b89792e981
Evgenii Kozlov <ubuphobos@gmail.com>
2020-11-12 14:34:49 +0200

    Design/search bar (#3)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,20 @@
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 fun main() = Window {
-    var text by remember { mutableStateOf("Say hello!") }
+
+    var query by remember { mutableStateOf("") }
 
     MaterialTheme(
         colors = MaterialTheme.colors.copy(
@@ -18,10 +25,23 @@ fun main() = Window {
         )
     ) {
         Scaffold {
-            Button(onClick = {
-                text = "Hello user"
-            }) {
-                Text(text)
+            TopAppBar(
+                    backgroundColor = Color(49,49,49),
+                    elevation = 3.dp
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = "Your search query: ", color = Color.White, fontSize = 12.sp)
+                    TextField(
+                            textStyle = TextStyle(
+                                    color = Color.White,
+                                    fontSize = 12.sp
+                            ),
+                            modifier = Modifier.weight(1f),
+                            maxLines = 1,
+                            onValueChange = { query = it },
+                            value = query
+                    )
+                }
             }
         }
     }
